2d6b5d424f49f7f2846023211ffe02ea4fa4e07c
DGM NWO <ss8167970@gmail.com>
2020-07-24 19:15:04 +0530

    Added another field called ID and removed Two information

--- Demo2.py
+++ Demo2.py
@@ -6,6 +6,6 @@ def Home():
     return render_template("home.html")
 @app.route("/about")
 def About():
-    return render_template("about.html")
+    return render_template("about2.html")
 if __name__=='__main__':
     app.run(debug=True)
