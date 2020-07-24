abeadbd9112b4c55b79b22cd6ff92a104661b123
DGM NWO <ss8167970@gmail.com>
2020-07-24 18:51:14 +0530

    creating Demo2 file with html files

---
+++ Demo2.py
@@ -0,0 +1,11 @@
+from flask import Flask,render_template
+app=Flask(__name__)
+@app.route("/")
+@app.route("/home")
+def Home():
+    return render_template("home.html")
+@app.route("/about")
+def About():
+    return render_template("about.html")
+if __name__=='__main__':
+    app.run(debug=True)
